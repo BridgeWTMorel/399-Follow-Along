2ba7d39739dd45d3061769f0cf146c51dbbd6447
BridgeWTMorel <tmorel@student.bridgew.edu>
2023-02-21 15:12:22 -0500

    initial commit

---
+++ Assets/SphereMove.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SphereMove : MonoBehaviour
+{
+    public int score = 0;
+    public TMP_Text scoreText;
+    public GameObject pauseObject;
+    private bool countdown;
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        countdown = false;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10 * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            if(Time.timeScale == 0) {
+                Time.timeScale = 1;
+                pauseObject.SetActive(false);
+            } else {
+                Time.timeScale = 0;
+                pauseObject.SetActive(true);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            countdown = true;
+        }
+        if(countdown) {
+            if(timer < 3) {
+                timer += Time.deltaTime;
+            } else {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+        }
+        float horizMove = Input.GetAxisRaw("Horizontal") * speed;
+        float vertMove = Input.GetAxisRaw("Vertical") * speed;
+        transform.position += new Vector3(horizMove, 0, vertMove);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger!");
+        float newX = Random.Range(-12, 12);
+        float newZ = Random.Range(-5, 5);
+        Debug.Log(newX);
+        Debug.Log(newZ);
+        other.transform.position = new Vector3(newX, 0, newZ);
+        score++;
+        scoreText.text = "Score: " + score;
+        Debug.Log("Score is now: " + score);
+    }
+}
